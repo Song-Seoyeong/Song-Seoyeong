dadf8938e7da583ef8d8e363e0afaa79eb3a8896
seoyeong <tjdud654@naver.com>
2024-08-28 14:27:20 +0900

    class component

---
+++ componet-test/src/components/ClassComponent.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class test extends Component{
+    render(){
+        return <div>클래스형 글래스입니다 하하</div>
+    }
+}
+
+export default test;
---
+++ componet-test/src/components/FuncComponent.js
@@ -0,0 +1,6 @@
+const test= () =>{
+    return <div>함수형입니다 제 동생은 함수입니다</div>
+}
+
+export default test;
+// default는 생략이 가능하다 대신 중괄호를 넣어야함 export {test}; >> 이름을 변경할 수 없다는 뜻이다 import할때 이름을 동일하게 사용해야한다
